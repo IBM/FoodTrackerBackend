d0483d9d80c801956beddcbd46c6f3d1c5eedfed
seabaylea <baileyc@uk.ibm.com>
2017-10-30 09:48:17 +0000

    Initial commit of FoodTrackerBackend tutorial

---
+++ iOS/FoodTracker/FoodTracker/Meal.swift
@@ -0,0 +1,45 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Jane Appleseed on 11/10/16.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Meal: Codable {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: Data
+    var rating: Int
+    
+    //MARK: Initialization
+    
+    init?(name: String, photo: Data, rating: Int) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0  {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+    }
+}
+
